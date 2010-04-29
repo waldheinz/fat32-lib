cd01b86b528776981acb72ce331396d60080b3e0
Matthias Treydte <waldheinz@gmail.com>
2010-04-29 17:15:59 +0200

    Checking the file system type label in SuperFloppyFormatterTest.

--- src/test/java/de/waldheinz/fs/fat/SuperFloppyFormatterTest.java
+++ src/test/java/de/waldheinz/fs/fat/SuperFloppyFormatterTest.java
@@ -6,6 +6,7 @@ import de.waldheinz.fs.util.FileDisk;
 import de.waldheinz.fs.util.RamDisk;
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -40,18 +41,29 @@ public class SuperFloppyFormatterTest {
     public void testFat12FormatValid() throws IOException {
         System.out.println("fat12Format (valid)");
 
-        BlockDevice dev = new RamDisk(16700000);
+        RamDisk dev = new RamDisk(16700000);
         
         FatFileSystem fs = SuperFloppyFormatter.get(dev).
                 setFatType(FatType.FAT12).format();
-        final BootSector bs = fs.getBootSector();
+
+        assertTrue (fs.getBootSector() instanceof Fat16BootSector);
+        final Fat16BootSector bs = (Fat16BootSector) fs.getBootSector();
+        
         System.out.println("sectors=" + bs.getNrTotalSectors());
         System.out.println("sectors per cluster=" + bs.getSectorsPerCluster());
         System.out.println("clusters=" +
                 bs.getNrTotalSectors() / bs.getSectorsPerCluster());
-        assertEquals(FatType.FAT12, fs.getFatType());
-    }
 
+        ByteBuffer b = dev.getBuffer();
+        b.position(0);
+        b.limit(512);
+        
+        Utils.hexDump(System.out, b);
+        
+        assertEquals(FatType.FAT12, fs.getFatType());
+        assertEquals("FAT12   ", bs.getFileSystemTypeLabel());
+    }
+    
     @Test(expected=IllegalArgumentException.class)
     public void testFat16FormatTooSmall() throws IOException {
         System.out.println("fat16Format (too small)");
@@ -65,7 +77,11 @@ public class SuperFloppyFormatterTest {
         System.out.println("fat16Format (valid)");
         
         BlockDevice dev = new RamDisk(16 * 1024 * 1024);
-        SuperFloppyFormatter.get(dev).setFatType(FatType.FAT16);
+        final FatFileSystem fs = SuperFloppyFormatter.get(dev).
+                setFatType(FatType.FAT16).format();
+
+        assertEquals(FatType.FAT16, fs.getFatType());
+        assertEquals("FAT16   ", fs.getBootSector().getFileSystemTypeLabel());
     }
     
     @Test(expected=IllegalArgumentException.class) @Ignore
@@ -96,7 +112,10 @@ public class SuperFloppyFormatterTest {
         BlockDevice dev = new RamDisk(50 * 1024 * 1024);
         FatFileSystem fs = SuperFloppyFormatter.get(dev).
                 setFatType(FatType.FAT32).format();
+        
         assertEquals(FatType.FAT32, fs.getFatType());
+        assertEquals("FAT32   ", fs.getBootSector().getFileSystemTypeLabel());
+
         fs.getRoot().addFile("this is another looooong name");
         fs.getRoot().addDirectory("this is a sub-directory!!!");
         fs.close();
