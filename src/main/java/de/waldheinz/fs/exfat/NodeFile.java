c2a59112dd5d2b4220263d46331ad2ae7db1fc9d
Matthias Treydte <waldheinz@gmail.com>
2010-12-26 13:45:51 +0100

    exfat: Added NodeFile stub.

---
+++ src/main/java/de/waldheinz/fs/exfat/NodeFile.java
@@ -0,0 +1,51 @@
+
+package de.waldheinz.fs.exfat;
+
+import de.waldheinz.fs.AbstractFsObject;
+import de.waldheinz.fs.FsFile;
+import de.waldheinz.fs.ReadOnlyException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/**
+ *
+ * @author Matthias Treydte &lt;waldheinz at gmail.com&gt;
+ */
+final class NodeFile extends AbstractFsObject implements FsFile {
+    
+    private final Node node;
+
+    public NodeFile(Node node, boolean readOnly) {
+        super(readOnly);
+        
+        this.node = node;
+    }
+    
+    @Override
+    public long getLength() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setLength(long length) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void read(long offset, ByteBuffer dest) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void write(long offset, ByteBuffer src)
+            throws ReadOnlyException, IOException {
+        
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void flush() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
