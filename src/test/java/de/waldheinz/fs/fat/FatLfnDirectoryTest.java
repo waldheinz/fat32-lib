39ba39454a64f90be862388b2222eab184d27f10
André Bierwolf <a.b.bierwolf@amc.uva.nl>
2014-07-31 10:25:03 +0200

    Restored static import for Assert

--- src/test/java/de/waldheinz/fs/fat/FatLfnDirectoryTest.java
+++ src/test/java/de/waldheinz/fs/fat/FatLfnDirectoryTest.java
@@ -18,12 +18,7 @@
 
 package de.waldheinz.fs.fat;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Date;
